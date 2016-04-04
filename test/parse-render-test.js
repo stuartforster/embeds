0878b726485177e15857eb2232d6f139aa8fa9e0
David Björklund <david.bjorklund@gmail.com>
2016-04-04 12:12:45 +0200

    add some parse + render integration tests

---
+++ test/parse-render-test.js
@@ -0,0 +1,40 @@
+import test from 'ava';
+import 'babel-core/register';
+import fixtures from './fixtures';
+import {render, parse} from '../lib';
+import {string} from 'deku';
+import queryDom from 'query-dom';
+
+const parseAndRender = input =>
+  string.render(render(parse(queryDom(input))));
+
+test('parse() + render() facebook - post', t => {
+  const input = fixtures.facebookPost;
+  const expected = input;
+  const actual = parseAndRender(input);
+  t.is(actual, expected);
+});
+
+test('parse() + render() facebook - video', t => {
+  const input = fixtures.facebookVideo;
+  const expected = input;
+  const actual = parseAndRender(input);
+  t.is(actual, expected);
+
+});
+
+test('parse() + render() instagram - with caption', t => {
+  const input = fixtures.instagramCaption;
+  const expected = input;
+  const actual = parseAndRender(input);
+  t.is(actual, expected);
+
+});
+
+test('parse() + render() instagram - without caption', t => {
+  const input = fixtures.instagramWithoutCaption;
+  const expected = input;
+  const actual = parseAndRender(input);
+  t.is(actual, expected);
+
+});
